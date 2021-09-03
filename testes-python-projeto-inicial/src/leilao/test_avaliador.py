2d946a37c5511a8852d41962183b28a9c2404f7c
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-09-03 17:23:24 -0300

    teste 2

--- testes-python-projeto-inicial/src/leilao/test_avaliador.py
+++ testes-python-projeto-inicial/src/leilao/test_avaliador.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
-from dominio import Avaliador, Lance, Leilao, Usuario
+from .dominio import Avaliador, Lance, Leilao, Usuario
 
 class TestAvaliador(TestCase):
 
-    def teste_avalia(self):
+    def teste_retorna_maior_menor_valor_adicionado_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -23,5 +23,39 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+    
+    def teste_retorna_maior_menor_valor_adicionado_descrecente(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
 
+        lance_do_yuri = Lance(yuri, 100)
+        lance_do_gui = Lance(gui, 150)
 
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+    
+    def teste_retorna_maior_menor_valor_apenas_um_lance(self):
+        gui = Usuario('Gui')
+        
+        lance_do_gui = Lance(gui, 150)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150, avaliador.maior_lance)
+        self.assertEqual(150, avaliador.menor_lance)
